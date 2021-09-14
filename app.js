038823831ee581720a1004de64ea583feecafeb0
Stanislav <verbatim8872@gmail.com>
2021-09-14 13:24:09 +0300

    add connect to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = config.get('port') || 5000;
 
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+
+const start = async () => {
+    try{
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            // useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+    }catch(e){
+        console.log(`Ooops, some error ---> ${e.message}`);
+        process.exit(1);
+    }
+}
+
+start();
