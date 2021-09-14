27cf45888b1b22e4412fac85c3f8931c01862df7
Stanislav <verbatim8872@gmail.com>
2021-09-14 18:34:08 +0300

    add login route

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { validationResult, check } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('../config/default.json');
 
 const router = Router();
 
@@ -39,8 +41,41 @@ router.post(
     }
 });
 
-router.post('/login', async (req, res) => {
+router.post(
+    '/login', 
+    [
+        check('email', 'Please enter correct email').normalizeEmail().isEmail(),
+        check('password', 'Email or password uncorrect entered').exists()
+    ],
+    async (req, res) => {
     try{
+        const { email, password } = req.body;
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array(), message: "Uncorrect login data" });
+        }
+
+        const user = User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isMatch = bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({ message: "Email or password entered uncorrect" });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({ token, userId: user.id });
 
     }catch(e){
         res.status(500).json({ message: `Oooops, some error ----> ${e.message}` });
