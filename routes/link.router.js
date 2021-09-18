5b43bc46a91cc0faf7748aa661813a5f5a9928cf
Stanislav <verbatim8872@gmail.com>
2021-09-18 13:10:01 +0300

    add auth middleware and fill link router

--- routes/link.router.js
+++ routes/link.router.js
@@ -1,29 +1,54 @@
 const { Router } = require('express');
+const Link = require('../models/Link');
+const auth = require('../middlewares/auth.middleware');
 const config = require('config');
+const shortid = require('shortid');
 
 const router = Router();
 
-router.post('/generate', async (req, res) => {
+router.post('/generate', auth, async (req, res) => {
     try{
+        const baseUrl = config.get('baseUrl');
+        const { from } = req.body;
+
+        const code = shortid.generate();
+
+        const to = baseUrl + '/t/' + code;
+
+        const exist = Link.findOne({ from });
+
+        if(exist){
+            return res.json(exist);
+        }
+
+        const link = new Link({
+            from, to, code, owner: req.user.userId
+        });
+
+        await link.save();
+
+        res.status(201).json({ link });
 
     }catch(e){
-        res.status(500).json({ message: 'Unknown error' })
+        res.status(500).json({ message: 'Unknown error' });
     }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
     try{
-
+        const links = await Link.find({ owner: req.user.userId });
+        res.json(links);
     }catch(e){
-        res.status(500).json({ message: 'Unknown error' })
+        res.status(500).json({ message: 'Unknown error' });
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
     try{
-
+        const link = await Link.findById(req.params.id);
+        res.json(link);
     }catch(e){
-        res.status(500).json({ message: 'Unknown error' })
+        res.status(500).json({ message: 'Unknown error' });
     }
 });
 
